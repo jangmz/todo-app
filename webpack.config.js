ead34badc778845fa61deeaafc4eb49653cab508
jangmz <131482887+jangmz@users.noreply.github.com>
2023-12-18 15:58:20 +0100

    created basic  folder structure and files, created index.html template, updated package.json and webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    devServer: {
+        static: "./dist",
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html",
+            inject: "head",
+            scriptLoading: "defer",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+};
